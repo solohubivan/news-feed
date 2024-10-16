d036afbe4d31e6d122cece784c9f3a011e35bc5e
solohubivan <solohubivan@gmail.com>
2024-10-16 13:07:22 +0300

    Initial Commit

---
+++ News feed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News feed
+//
+//  Created by Ivan Solohub on 16.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
