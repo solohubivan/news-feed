91ca7ba94fe00f4b82d59cb74cb67388bcc0197e
solohubivan <solohubivan@gmail.com>
2024-10-16 13:50:20 +0300

    initial commit

---
+++ News feed/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  News feed
+//
+//  Created by Ivan Solohub on 16.10.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ News feed/ViewControllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News feed
+//
+//  Created by Ivan Solohub on 16.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+// initial commit
+}
+
